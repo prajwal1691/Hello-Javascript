8da935774f7c3306bfc0e65032c5835a23ac8207
Prajwal J <118866033+prajwal1691@users.noreply.github.com>
2023-03-03 17:47:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,6 @@ const express = require('express');
 /* const mongodb = require('mongodb'); */
 const { MongoClient } = require("mongodb");
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
-
 
 dotenv.config();
 
